27aa271dc19985559794dd0a3006e1729f54b1bd
septianmalela <maulanas430@gmail.com>
2024-08-12 07:34:21 +0700

    simple templating

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,13 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+import 'jquery'
+import "popper"
+import "bootstrap"
+
+// template home
+import 'template/config'
+import 'template/helper'
+import 'template/menu'
+import 'template/main'
